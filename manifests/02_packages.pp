7991da412b2cc6b9d49aa658f3ac92add8c5ef96
Spencer Brower <foxtrotalphalima@gmail.com>
2020-03-25 16:13:49 -0400

    feat: Added common packages.

--- manifests/02_packages.pp
+++ manifests/02_packages.pp
@@ -4,6 +4,18 @@ package {'google chrome':
   name => 'googlechrome'
 }
 
+# IDEs
+package {'vscode':}
+
+# Chat
+package {'discord':}
+
+# Other
+package {'f.lux':}
+package {'joplin':}
+package {'teamviewer':
+  name => 'teamviewer9'
+}
 package {'virtualbox':
   ensure => '6.1.2'
 }
