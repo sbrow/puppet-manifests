e67e1422cbb0798856a6667a1579d93c8351d4d3
Spencer Brower <foxtrotalphalima@gmail.com>
2020-04-17 15:38:41 -0400

    fix: `CleanMyMac` now installs correctly.
    Signed-off-by: Spencer Brower <foxtrotalphalima@gmail.com>

--- manifests/mac.pp
+++ manifests/mac.pp
@@ -1,7 +1,9 @@
 class mac {
   package { 'cleanmymac':
     # Updates itself
-    ensure => present,
-    provider => homebrew
+    ensure => installed,
+    # Homebrew package is bugged.
+    provider => appdmg,
+    source => 'https://dl.devmate.com/com.macpaw.CleanMyMac4/CleanMyMacX.dmg',
   }
 }
