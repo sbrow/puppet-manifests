5a85c77c9837f0048f10b99ec11184ed31f5cd91
Spencer Brower <foxtrotalphalima@gmail.com>
2020-04-21 19:19:45 -0400

    feat: Added `gpg-suite` configuration to `mac.pp`.
    Signed-off-by: Spencer Brower <foxtrotalphalima@gmail.com>

--- manifests/mac.pp
+++ manifests/mac.pp
@@ -6,4 +6,13 @@ class mac {
     provider => appdmg,
     source => 'https://dl.devmate.com/com.macpaw.CleanMyMac4/CleanMyMacX.dmg',
   }
+
+  package { 'gpg-suite': }
+  file { '/Users/sbrow/.gnupg/gpg-agent.conf':
+    content => 'pinentry-program /usr/local/MacGPG2/libexec/pinentry-mac.app/Contents/MacOS/pinentry-mac
+enable-ssh-support
+default-cache-ttl 600
+max-cache-ttl 7200
+'
+  }
 }
